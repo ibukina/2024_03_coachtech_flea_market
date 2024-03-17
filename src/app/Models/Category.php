cfbd124fcbde5951434c0534bf4ca5e388ab5998
nakamura ibuki <trees.animal47@gmail.com>
2024-03-17 14:21:33 +0900

    modelとtableの作成

---
+++ src/app/Models/Category.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Category extends Model
+{
+    use HasFactory;
+
+    public function category_items(){
+        return $this->hasMany('App/Models/CategoryItem');
+    }
+
+    protected $fillable=[
+        'name'
+    ];
+}
---
+++ src/app/Models/Item.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    public function user(){
+        return $this->belongsTo('App/Models/User');
+    }
+
+    public function condition(){
+        return $this->belongsTo('App/Models/Condition');
+    }
+
+    public function category_items(){
+        return $this->hasMany('App/Models/CategoryItem');
+    }
+
+    public function likes(){
+        return $this->hasMany('App/Models/Like');
+    }
+
+    public function sold_items(){
+        return $this->hasMany('App/Models/SoldItem');
+    }
+
+    public function comments(){
+        return $this->hasMany('App/Models/Comment');
+    }
+
+    protected $fillable=[
+        'user_id',
+        'condition_id',
+        'category_item_id',
+        'name',
+        'price',
+        'description',
+        'img_url',
+    ];
+}
