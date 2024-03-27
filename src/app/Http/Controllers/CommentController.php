2bacf9d956ad9575c676deadc195b43e281f2d97
nakamura ibuki <trees.animal47@gmail.com>
2024-03-27 14:51:16 +0900

    usersテーブルとコントローラーの修正・リクエストの追加と作成

--- src/app/Http/Controllers/CommentController.php
+++ src/app/Http/Controllers/CommentController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\CommentRequest;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Comment;
 
 class CommentController extends Controller
 {
-    public function create($item_id){
+    public function create(CommentRequest $request, $item_id){
+        $user_id=Auth::id();
+        Comment::create([
+            'user_id'=>$user_id,
+            'item_id'=>$item_id,
+            'comment'=>$request['comment'],
+        ]);
         return redirect('/');
     }
 }
--- src/app/Http/Controllers/PurchaseController.php
+++ src/app/Http/Controllers/PurchaseController.php
@@ -3,16 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AddressRequest;
+use Illuminate\Support\Facades\Auth;
+use Ap\Models\Profile;
 use App\Models\Item;
 use App\Models\Purchase;
+use App\Models\SoldItem;
 
 class PurchaseController extends Controller
 {
-    public function index($item_id){
+    public function index(){
+        // $items=Item::find($item_id);
         return view('purchase');
     }
 
     public function purchase(Request $request){
+        $user_id=Auth::id();
+        SoldItem::create([
+            'user_id'=>$user_id,
+            'item_id'=>$request['item_id'],
+        ]);
         return redirect('/');
     }
 
@@ -20,7 +30,13 @@ class PurchaseController extends Controller
         return view('address');
     }
 
-    public function updateAddress(Request $request){
-        return redirect('/');
+    public function updateAddress(AddressRequest $request){
+        $user_id=Auth::user();
+        Profile::where('user_id', $user_id)->update([
+            'postcode'=>$request['postcode'],
+            'address'=>$request['address'],
+            'building'=>$request['building'],
+        ]);
+        return redirect('/purchase/item_id');
     }
 }
