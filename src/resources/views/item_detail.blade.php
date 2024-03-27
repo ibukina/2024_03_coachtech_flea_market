aea69edd101dac07b6a5eb5f3dca9ddcdec003dd
nakamura ibuki <trees.animal47@gmail.com>
2024-03-27 14:55:30 +0900

    ホームページとそのcssの修正・商品詳細ページの修正とcss作成・common.cssの修正

--- src/resources/views/item_detail.blade.php
+++ src/resources/views/item_detail.blade.php
@@ -7,47 +7,54 @@
 @section('main_content')
 <div class="content-detail">
     <div class="item-image_wrapper">
-        <div class="item-image">商品画像</div>
+        <div class="item-image">
+            <img src="" alt="商品画像">
+        </div>
     </div>
     <div class="item-detail_wrapper">
-        <h2 class="item-name">商品名</h2>
-        <div class="item-brand">ブランド名</div>
-        <div class="price">¥47,000(値段)</div>
-        <div class="mark-container">
-            <div class="mark-wrapper">
-                <form class="like-form" action="" method="">
-                    @csrf
-                    <button class="like-button"></button>
-                </form>
-                <div class="mark-number">3</div>
+        <div class="item-detail">
+            <h2 class="item-name">商品名</h2>
+            <div class="item-brand">ブランド名</div>
+            <div class="item-price">¥47,000(値段)</div>
+            <div class="mark-container">
+                <div class="mark-wrapper">
+                    <form class="like-form" action="" method="">
+                        @csrf
+                        <input type="hidden" name="item_id" value="">
+                        <button class="like-button"></button>
+                    </form>
+                    <div class="mark-number">3</div>
+                </div>
+                <div class="mark-wrapper">
+                    <form class="comment-form" action="" method="">
+                        @csrf
+                        <input type="hidden" name="item_id" value="">
+                        <button class="comment-button"></button>
+                    </form>
+                    <div class="mark-number">14</div>
+                </div>
             </div>
-            <div class="mark-wrapper">
-                <form class="comment-form" action="" method="">
-                    @csrf
-                    <button class="comment-button"></button>
-                </form>
-                <div class="mark-number">14</div>
+            <form class="purchase-form" action="/purchase/item_id" method="get">
+                @csrf
+                <input type="hidden" name="item_id" value="">
+                <button class="purchase-button">購入する</button>
+            </form>
+            <h3 class="description-title">商品説明</h3>
+            <p class="item-color">カラー：グレー</p>
+            <p class="item-description">新品</br>
+                商品の状態は良好です。傷もありません。</br></br>
+                購入後、即発送いたします。
+            </p>
+            <h3 class="information-title">商品の情報</h3>
+            <div class="category-wrapper">
+                <div class="category-title">カテゴリー</div>
+                <div class="category-tag">洋服</div>
+                <div class="category-tag">メンズ</div>
             </div>
-        </div>
-        <form class="purchase-form" action="" method="">
-            @csrf
-            <button class="purchase-button">購入する</button>
-        </form>
-        <h3 class="summary-title">商品説明</h3>
-        <p class="item-color">カラー：グレー</p>
-        <p class="item-summary">新品
-            商品の状態は良好です。傷もありません。
-            購入後、即発送いたします。
-        </p>
-        <h3 class="information-title">商品の情報</h3>
-        <div class="category-wrapper">
-            <div class="category-title">カテゴリー</div>
-            <div class="category-tag">洋服</div>
-            <div class="category-tag">メンズ</div>
-        </div>
-        <div class="condition-wrapper">
-            <div class="condition-title">商品の状態</div>
-            <div class="condition-tag">良好</div>
+            <div class="condition-wrapper">
+                <div class="condition-title">商品の状態</div>
+                <div class="condition-tag">良好</div>
+            </div>
         </div>
     </div>
 </div>
